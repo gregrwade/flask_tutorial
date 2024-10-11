564dc2c1f8afa7d36177f78a8fec30b1a8a4df6c
Greg Wade <greg.r.wade@gmail.com>
2024-10-11 16:09:11 -0400

    required config file

--- core/__init__.py
+++ core/__init__.py
@@ -15,7 +15,7 @@ def create_app(test_config=None):
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
+        app.config.from_pyfile("config.py", silent=False)
         
         # output the configuration values
         for key, value in app.config.items():
