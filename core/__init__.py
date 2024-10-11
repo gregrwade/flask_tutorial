a1c7a3089dae90724fcb159b6a56b5d451c3774d
Greg Wade <greg.r.wade@gmail.com>
2024-10-11 16:21:58 -0400

    removed config.py default

--- core/__init__.py
+++ core/__init__.py
@@ -15,7 +15,7 @@ def create_app(test_config=None):
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=False)
+        app.config.from_pyfile("config.py", silent=True)
         
         # output the configuration values
         for key, value in app.config.items():
